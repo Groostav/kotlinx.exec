6d25184c15c8885022df15f0e6e8a20fa0bca2f4
groostav <geoff.groos@empowerops.com>
2018-08-07 21:29:17 -0700

    well this is a thing

---
+++ jdk8/src/main/kotlin/groostav/kotlinx/exec/TestClass.kt
@@ -0,0 +1,8 @@
+package groostav.kotlinx.exec
+
+class TestClass {
+
+    val x = 4;
+
+    public inline fun stupidThingForWarning(){}
+}
--- jdk8/src/main/kotlin/groostav/kotlinx/exec/ZTProcessFacade.kt
+++ jdk8/src/main/kotlin/groostav/kotlinx/exec/ZTProcessFacade.kt
@@ -4,49 +4,49 @@ import org.zeroturnaround.process.Processes
 import org.zeroturnaround.process.WindowsProcess
 
 internal class ZeroTurnaroundProcessFacade(val process: Process, pid: Int): ProcessControlFacade {
-
-    init {
-        if(JavaVersion >= 9) trace { "WARN: using ZeroTurnaroundProcess on Java 9+" }
-    }
-
-    companion object: ProcessControlFacade.Factory  {
-        val ztOnClassPath: Boolean = Try { Class.forName("org.zeroturnaround.process.Processes") } != null
-        override fun create(process: Process, pid: Int) = supportedIf(ztOnClassPath) { ZeroTurnaroundProcessFacade(process, pid) }
-    }
-
-    private val pidProcess = Processes.newPidProcess(pid)
-
-    override fun tryKillGracefullyAsync(includeDescendants: Boolean): Maybe<Unit> {
-
-        when(pidProcess){
-            is WindowsProcess -> {
-                pidProcess.isGracefulDestroyEnabled = true
-                pidProcess.isIncludeChildren = includeDescendants
-            }
-            else -> {
-                //can we simply issue a pgrep -P call here?
-                if(includeDescendants) { return Unsupported }
-            }
-        }
-
-        pidProcess.destroyGracefully()
-
-        return Supported(Unit)
-    }
-
-    override fun killForcefullyAsync(includeDescendants: Boolean): Maybe<Unit> {
-
-        when(pidProcess){
-            is WindowsProcess -> {
-                pidProcess.isIncludeChildren = includeDescendants
-            }
-            else -> {
-                if(includeDescendants) { return Unsupported }
-            }
-        }
-
-        pidProcess.destroyForcefully()
-
-        return Supported(Unit)
-    }
+//
+//    init {
+//        if(JavaVersion >= 9) trace { "WARN: using ZeroTurnaroundProcess on Java 9+" }
+//    }
+//
+//    companion object: ProcessControlFacade.Factory  {
+//        val ztOnClassPath: Boolean = Try { Class.forName("org.zeroturnaround.process.Processes") } != null
+//        override fun create(process: Process, pid: Int) = supportedIf(ztOnClassPath) { ZeroTurnaroundProcessFacade(process, pid) }
+//    }
+//
+//    private val pidProcess = Processes.newPidProcess(pid)
+//
+//    override fun tryKillGracefullyAsync(includeDescendants: Boolean): Maybe<Unit> {
+//
+//        when(pidProcess){
+//            is WindowsProcess -> {
+//                pidProcess.isGracefulDestroyEnabled = true
+//                pidProcess.isIncludeChildren = includeDescendants
+//            }
+//            else -> {
+//                //can we simply issue a pgrep -P call here?
+//                if(includeDescendants) { return Unsupported }
+//            }
+//        }
+//
+//        pidProcess.destroyGracefully()
+//
+//        return Supported(Unit)
+//    }
+//
+//    override fun killForcefullyAsync(includeDescendants: Boolean): Maybe<Unit> {
+//
+//        when(pidProcess){
+//            is WindowsProcess -> {
+//                pidProcess.isIncludeChildren = includeDescendants
+//            }
+//            else -> {
+//                if(includeDescendants) { return Unsupported }
+//            }
+//        }
+//
+//        pidProcess.destroyForcefully()
+//
+//        return Supported(Unit)
+//    }
 }
