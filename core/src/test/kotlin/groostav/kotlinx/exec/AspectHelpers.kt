6a2ea7cee2a6cb368e3605cd6d23a5bf286e147b
Geoff <geoff.groos@empowerops.com>
2019-02-07 15:33:48 -0800

    yeah, Im not quite getting the rewriting done when I need it i guess

--- core/src/test/kotlin/groostav/kotlinx/exec/AspectHelpers.kt
+++ core/src/test/kotlin/groostav/kotlinx/exec/AspectHelpers.kt
@@ -1,22 +1,34 @@
 package groostav.kotlinx.exec
 
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
 import org.junit.Test
 
 
-class AspectHelpers {
-    @Test fun `stuff`(): Unit = TODO("""
-        Can we apply some aspect oriented programming techniques to help us cover some things?
-        For example, buffers (or threads, in the case of blocking listeners) must be acquired before the `start()` call
-        else we might lose some data from the process.
-        Can we insert some kind of telemetry to track buffer allocations, thread allocations, and start() calls,
-        then assert on them in some kind of shared way?
+open class AspectHelpers {
 
-        what needs to be covered:
-        1. as above, buffer allocation happens before pb.start() call
-        2. as above, thread allocation happens before pb.start() call
-        3. that `process.toList()` always complete(d) before `process.join()`
-           -- because the aggregate channel should always complete before the exit value
-        4. if the process joined, its PID should not be listed as running by the OS
+    @lazy_list open fun thingy(): List<Int> {
+        return listOf(1, 2, 3)
+    }
 
-    """.trimIndent())
+    @Test fun `stuff`(): Unit{
+        val x = MyAspect();
+        val result= thingy()
+
+        TODO();
+    }
+}
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FUNCTION)
+annotation class lazy_list
+
+@Aspect
+class MyAspect {
+
+    @Pointcut("execution(@annotation.lazy_list * *(..))")
+    fun doPOintCut(){
+        println("yarr!! we're on the class path me harties!!")
+    }
+
 }
